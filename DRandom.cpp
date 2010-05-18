d9835f3a80bde6797fe1638b0036132204b2050b
David Coss <david@davecoss.com>
2010-05-18 17:43:27 -0500

    Added MersenneTwister.h as the new (GPL compliant) random number generator. DRandom now acts as a wrapper for this class, guaranteeing backwards-compatibility.

--- DRandom.cpp
+++ DRandom.cpp
@@ -7,62 +7,35 @@ namespace utils
 
   DRandom::DRandom()
   {
-    mySeed = new long;
-    changeSeed(42L);
-    random();
+    rng = new MTRand();
+    mySeed = new long();
+    *mySeed = 42;
   }
 
   DRandom::~DRandom()
   {
-  
+    delete rng; rng = 0;
+    delete mySeed; mySeed = 0;
   }
 
   double DRandom::random()
   {
-    int j;
-    long k;
-    static long iy=0;
-    static long iv[NTAB];
-    float temp;
-
-    if(*mySeed <= 0 || !iy)
-      {
-	if(-(*mySeed) < 1)
-	  *mySeed = 1;
-	else
-	  *mySeed = -(*mySeed);
-	
-	for(j=NTAB+7;j>=0;j--)
-	  {
-	    k=(*mySeed)/IQ;
-	    *mySeed=IA*(*mySeed - k*IQ)-IR*k;
-	    if(*mySeed < 0)
-	      *mySeed += IM;
-	    if(j<NTAB)
-	      iv[j] = *mySeed;
-	    
-	  }
-	iy = iv[0];
-      }
+    if(rng == 0)
+      rng = new MTRand();
     
-    k = (*mySeed)/IQ;
-    *mySeed = IA*(*mySeed - k*IQ)-IR*k;
-    if(*mySeed < 0)
-      *mySeed += IM;
-    j = iy/NDIV;
-    iy = iv[j];
-    iv[j] = *mySeed;
-    if((temp = AM*iy) > RNMX)
-      return RNMX;
-    else
-      return temp;
+    return rng->randDblExc();
   }
 
   long DRandom::changeSeed(long seed)
   {
-    int returnMe = *mySeed;
-    *mySeed = -1L*seed;
-    return -1L*returnMe;
+    long returnMe = 0;
+    if(mySeed != 0)
+      returnMe = *mySeed;
+    if(rng == 0)
+      rng = new MTRand();
+
+    rng->seed(seed);
+    return returnMe;
   }
 
 
--- DRandom.h
+++ DRandom.h
@@ -1,26 +1,22 @@
 #ifndef DRANDOM_CPP
 #define DRANDOM_CPP
 
-//Version 2.0
-
-//Defines needed for random
-#define IA 16807
-#define IM 2147483647
-#define AM (1.0/IM)
-#define IQ 127773
-#define IR 2836
-#define NTAB 32
-#define NDIV (1+(IM-1)/NTAB)
-#define EPS 1.2e-7
-#define RNMX (1.0-EPS)
+#include "MersenneTwister.h"
 
 namespace utils
 {
 
   /**
-   * My Random Number Generator
-   * Returns a uniform deviate in the interval (0,1).
-   * Created By David Coss, 2008 based on "ran1" from Numerical Recipes in C 1992.
+   * Random Number Generator
+   * Uses MerseeneTwister.h (see that file for its copyright notice).
+   * This class was originally created but was not GPL compliant.
+   * For backwards-compatibility with applications that still use DRandom, 
+   * this file is included, acting as a wrapper for a GPL compliant 
+   * random number generator. With that in mind, MTRand should be
+   * used directly in the future.
+   *
+   * Created By David Coss, 2010
+   * Copying and distributing are permitted under the terms of the GNU Public License version 3(see COPYING or http://www.gnu.org/licenses/gpl.txt).
    */
   class DRandom{
 
@@ -53,6 +49,7 @@ namespace utils
 
   private:	
     long * mySeed;
+    MTRand * rng;
   };
 
 
