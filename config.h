941bbfeadbb1ec8dd77263e63ec67bf01c535fef
David Coss <David.Coss@stjude.org>
2012-12-28 16:16:47 -0600

    Started to convert to CMake

--- config.h
+++ config.h
@@ -1,80 +1,11 @@
-/* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.ac by autoheader.  */
-
-/* Define to 1 if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H 1
-
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
-/* Define to 1 if you have the `modf' function. */
-#define HAVE_MODF 1
-
-/* Define to 1 if you have the `pow' function. */
-/* #undef HAVE_POW */
-
-/* Define to 1 if you have the `sqrt' function. */
-/* #undef HAVE_SQRT */
-
-/* Define to 1 if stdbool.h conforms to C99. */
-#define HAVE_STDBOOL_H 1
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
-
-/* Define to 1 if you have the `strchr' function. */
-#define HAVE_STRCHR 1
-
-/* Define to 1 if you have the <strings.h> header file. */
-#define HAVE_STRINGS_H 1
-
-/* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
-
-/* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
-
-/* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
-
-/* Define to 1 if the system has the type `_Bool'. */
-#define HAVE__BOOL 1
-
-/* Name of package */
-#define PACKAGE "dnstd"
-
-/* Define to the address where bug reports for this package should be sent. */
+#define PROJECT_NAME "DNSTD"
+#define PACKAGE "DNSTD"
+#define PACKAGE_NAME "DNSTD"
 #define PACKAGE_BUGREPORT "david@davecoss.com"
 
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "DNSTD"
+#define DNSTD_MAJOR_VERSION 2
+#define DNSTD_MINOR_VERSION 3
+#define VERSION "2.3"
+#define PACKAGE_STRING PACKAGE_NAME " " VERSION
 
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "DNSTD 2.3.1"
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "dnstd"
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "2.3.1"
-
-/* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
-
-/* Version number of package */
-#define VERSION "2.3.1"
-
-/* Define to `__inline__' or `__inline' if that's what the C compiler
-   calls it, or to nothing if 'inline' is not supported under any name.  */
-#ifndef __cplusplus
-/* #undef inline */
-#endif
-
-/* Define to `unsigned int' if <sys/types.h> does not define. */
-/* #undef size_t */
+/* #undef BUILD_WITH_ZLIB */
